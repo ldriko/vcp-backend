83e3d721d39240a67e8a4ae94478aa395696fafb
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-09-24 17:21:52 +0700

    Rearrange api routes Group routes by middleware

--- routes/v1.php
+++ routes/v1.php
@@ -4,22 +4,13 @@ use App\Http\Controllers\v1\JournalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+require __DIR__ . '/auth.php';
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', fn (Request $request) => $request->user());
 
-Route::prefix('/journals')->group(function () {
-    Route::post('', [JournalController::class, 'store']);
-    Route::put('/{journal}', [JournalController::class, 'update']);
+    Route::prefix('/journals')->group(function () {
+        Route::post('', [JournalController::class, 'store']);
+        Route::put('/{journal}', [JournalController::class, 'update']);
+    });
 });
