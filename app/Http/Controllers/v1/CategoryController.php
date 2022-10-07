e4264adeba4962b3d5ce931aaa179adbb9f87fa5
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-10-08 00:24:17 +0700

    Add categories functionality

--- app/Http/Controllers/v1/CategoryController.php
+++ app/Http/Controllers/v1/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,76 +12,30 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
+     * @param Request $request
      *
-     * @return \Illuminate\Http\Response
+     * @return Collection
      */
-    public function create()
+    public function index(Request $request): Collection
     {
-        //
-    }
+        $query = Category::query();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        if ($request->q) {
+            $query->where('name', 'like', $request->q);
+        }
+
+        return $query->get();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
+     * @param Category $category
      *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return Category
      */
-    public function edit(Category $category)
+    public function show(Category $category): Category
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
-    {
-        //
+        return $category;
     }
 }
--- routes/v1/v1.php
+++ routes/v1/v1.php
@@ -1,19 +1,21 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\v1\GroupAttachmentController;
-use App\Http\Controllers\v1\GroupChatController;
-use App\Http\Controllers\v1\GroupController;
-use App\Http\Controllers\v1\GroupMemberController;
-use App\Http\Controllers\v1\JournalController;
+use App\Http\Controllers\v1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-require __DIR__ . '/auth.php';
-require __DIR__ . '/journals.php';
-require __DIR__ . '/groups.php';
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn(Request $request) => $request->user());
     Route::put('/user', [RegisteredUserController::class, 'update']);
 });
+
+Route::prefix('categories')->group(function () {
+    Route::get('', [CategoryController::class, 'index']);
+    Route::get('/{category}', [CategoryController::class, 'show']);
+});
+
+require __DIR__ . '/auth.php';
+require __DIR__ . '/journals.php';
+require __DIR__ . '/groups.php';
+
