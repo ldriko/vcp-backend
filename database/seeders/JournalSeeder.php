4371cefdb9ba9a77d2b214934b9624419bf53aae
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-10-18 17:50:01 +0700

    add: add categories to journal seeder

--- database/seeders/JournalSeeder.php
+++ database/seeders/JournalSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Journal;
+use App\Models\JournalCategory;
 use Illuminate\Database\Seeder;
 
 class JournalSeeder extends Seeder
@@ -12,11 +13,24 @@ class JournalSeeder extends Seeder
      */
     public function run()
     {
-        Journal::factory()->create(['title' => 'Arsitektur IBM System/370', 'user_id' => 1]);
-        Journal::factory()->create(['title' => 'Desain Terasering', 'user_id' => 1]);
-        Journal::factory()->create(['title' => 'Diagnosis Modern GERD', 'user_id' => 2]);
-        Journal::factory()->create(['title' => 'Tesla: Kendaraan Tenaga Listrik', 'user_id' => 2]);
-        Journal::factory()->create(['title' => 'Pengaruh Pemanasan Global Pada Kehidupan Laut', 'user_id' => 3]);
-        Journal::factory()->create(['title' => 'Implementasi Teknologi Blockchain Pada Pendidikan', 'user_id' => 3]);
+        $journals = [
+            ['title' => 'Arsitektur IBM System/370', 'user_id' => 1, 'categories' => [7, 8]],
+            ['title' => 'Desain Terasering', 'user_id' => 1, 'categories' => [1, 9, 10]],
+            ['title' => 'Diagnosis Modern GERD', 'user_id' => 2, 'categories' => [7, 9]],
+            ['title' => 'Tesla: Kendaraan Tenaga Listrik', 'user_id' => 2, 'categories' => [7, 8, 9, 10]],
+            ['title' => 'Pengaruh Pemanasan Global Pada Kehidupan Laut', 'user_id' => 3, 'categories' => [9]],
+            ['title' => 'Implementasi Teknologi Blockchain Pada Pendidikan', 'user_id' => 3, 'categories' => [2, 4, 7]]
+        ];
+
+        foreach ($journals as $journalValue) {
+            $journal = Journal::factory()->create([
+                'title' => $journalValue['title'],
+                'user_id' => $journalValue['user_id']
+            ]);
+
+            foreach ($journalValue['categories'] as $category) {
+                JournalCategory::query()->create(['journal_code' => $journal->code, 'category_id' => $category]);
+            }
+        }
     }
 }
